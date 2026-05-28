5747c798681d7f3896e5c94d8e5a6691af83da90
sld0ut <hyejin.x919@gmail.com>
2026-05-28 16:36:30 +0900

    Create insert_buffer.tcl

---
+++ fe/sta/scr/insert_buffer.tcl
@@ -0,0 +1,5 @@
+###############################################################################
+# Change list, formatted for IC Compiler
+###############################################################################
+#current_instance
+#insert_buffer [get_ports {SCLK}] sibufx1 -new_net_names {net1} -new_cell_names {U1}
