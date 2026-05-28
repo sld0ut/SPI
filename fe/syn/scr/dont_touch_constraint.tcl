24bbba98a78404f3916cd435f956c615cf6b1bf6
sld0ut <hyejin.x919@gmail.com>
2026-05-28 16:38:56 +0900

    Create dont_touch_constraint.tcl

---
+++ fe/syn/scr/dont_touch_constraint.tcl
@@ -0,0 +1,5 @@
+#-------------------------------------------------#
+# set_dont_touch on hand instantiated cells
+#-------------------------------------------------#
+#set_dont_touch [get_cells -hier *DONT_TOUCH*] true
+#set_dont_touch [get_cells -hier *dont_touch*] true
